e6dd642a2041f0eabe3b83936bde3ee2f2fd6c9e
ChangSol <ckdwnsla12@naver.com>
2023-04-13 13:59:35 +0900

    feature Api Module add

---
+++ modules/module-api/src/main/java/org/changsol/ModuleApiApp.java
@@ -0,0 +1,15 @@
+package org.changsol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SrpingBoot 시작
+ */
+@SpringBootApplication
+public class ModuleApiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleApiApp.class, args);
+    }
+}
+
