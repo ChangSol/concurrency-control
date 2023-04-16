e6fafb24d32e49f3cb06a36eb3f6f1ca81319637
ChangSol <ckdwnsla12@naver.com>
2023-04-16 21:00:31 +0900

    fix swagger api response json parse error -> mediaType change

--- modules/module-api/src/main/java/org/changsol/apps/seqs/controller/SeqController.java
+++ modules/module-api/src/main/java/org/changsol/apps/seqs/controller/SeqController.java
@@ -1,9 +1,12 @@
 package org.changsol.apps.seqs.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.changsol.apps.seqs.service.SeqService;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +21,10 @@ public class SeqController {
 
 	private final SeqService seqService;
 
-	@Operation(summary = "주문 채번", description = "주문 채번 API입니다. 채번 양식 : ORD-2023-00001")
+	@Operation(summary = "주문 채번", description = "주문 채번 API입니다. 채번 양식 : ORD-2023-00001",
+		responses = {
+			@ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE))
+		})
 	@GetMapping
 	public ResponseEntity<String> getOrderSeq() {
 		return ResponseEntity.ok(seqService.getOrderSeq());
